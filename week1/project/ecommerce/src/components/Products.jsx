e8fc4356cd1dc20623da21d668233107ba545c11
konjit <kkll6349@gmail.com>
2025-04-22 14:13:24 +0200

    Make changes based on the review for styling and Products component.

--- week1/project/ecommerce/src/components/Products.jsx
+++ week1/project/ecommerce/src/components/Products.jsx
@@ -3,18 +3,17 @@ import Product from "./Product";
 import products from "../fake-data/all-products";
 
 const Products = ({ selectedCategory }) => {
+  const filteredProducts = !selectedCategory
+    ? products
+    : products.filter(
+        (product) => product.category === selectedCategory.replace("FAKE: ", "")
+      );
+
   return (
     <div className="products">
-      {
-        selectedCategory === null
-        ? products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))
-        : products
-            .filter(
-              (product) => product.category === selectedCategory.replace("FAKE: ", "")
-            )
-            .map((product) => <Product key={product.id} product={product} />)}
+      {filteredProducts.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 };
